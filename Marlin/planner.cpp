5d2fd07969639a317854954f13eb97fa117b065f
Jim Brown <jb.github@rcairgallery.com>
2017-12-06 13:23:08 -0500

    Revert "Revert "Merge remote-tracking branch 'upstream/bugfix-1.1.x' into EasyConfig-1.1.x""
    This reverts commit 54f14353e28ae12114e445651a861e6833de0c1e.

--- Marlin/planner.cpp
+++ Marlin/planner.cpp
@@ -132,11 +132,7 @@ float Planner::min_feedrate_mm_s,
     float Planner::xy_skew_factor;
     #if ENABLED(SKEW_CORRECTION_FOR_Z)
       float Planner::xz_skew_factor, Planner::yz_skew_factor;
-    #else
-      constexpr float Planner::xz_skew_factor, Planner::yz_skew_factor;
     #endif
-  #else
-    constexpr float Planner::xy_skew_factor, Planner::xz_skew_factor, Planner::yz_skew_factor;
   #endif
 #endif
 
@@ -578,8 +574,6 @@ void Planner::calculate_volumetric_multipliers() {
           rx = temprx;
           ry = tempry;
         }
-        else
-          SERIAL_ECHOLN(MSG_SKEW_WARN);
       }
     #endif
 
